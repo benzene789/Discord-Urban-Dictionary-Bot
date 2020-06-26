fbbe9fec7398165b95f03da65450a788129ad619
Ben Ridings <benridings@hotmail,com>
2020-06-26 21:57:59 +0100

    Add the search function to the bot

--- urbandictionarybot.py
+++ urbandictionarybot.py
@@ -1,23 +1,29 @@
 import requests
+from discord.ext import commands
+
 
 def search_query(query):
 
     url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
 
     headers = {
-    'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
-    'x-rapidapi-key': "53d82afdb9msh5305c311469a516p173941jsna60a3ec2ae11"
+        'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
+        'x-rapidapi-key': "53d82afdb9msh5305c311469a516p173941jsna60a3ec2ae11"
     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
 
-    print(response.text)
+    return response.text
 
-def main():
-    print("What do you want to search")
-    query = input()
-    querystring = {"term":query}
+bot = commands.Bot(command_prefix='=')
 
-    search_query(querystring)
 
-main()
+@bot.command(name='search')
+async def search_dictionary(ctx, *, query):
+    querystring = {"term": query}
+    result = search_query(querystring)
+
+    await ctx.send(result)
+
+    
