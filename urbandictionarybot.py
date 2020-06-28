2ffb2b712218002b9c19f077bb3901468856b2f0
Ben Ridings <benridings@hotmail,com>
2020-06-28 16:28:19 +0100

    Cleaned everything up and commented it

--- urbandictionarybot.py
+++ urbandictionarybot.py
@@ -4,47 +4,60 @@ import json
 import aiohttp
 from discord.ext import commands
 
+# Constants
+LIST = 'list'
+DEFINTION = 'definition'
+EXAMPLE = 'example'
+
+# Search a word the user types in
 async def search_query(querystring):
     async with aiohttp.ClientSession() as session:
         data = await fetch(session, f'https://api.urbandictionary.com/v0/define?term={querystring}')
         return data
-        
+
+# Fetch the URL
 async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
+# Search up a random word
 async def search_random_word():
     async with aiohttp.ClientSession() as session:
         data = await fetch(session, 'https://api.urbandictionary.com/v0/random')
         return data
 
+# Create the bot
 bot = commands.Bot(command_prefix='=')
 
+# Parse the searched word and display here
 @bot.command(name='search')
 async def search_dictionary(ctx, *, query):
+    # Gets the typed in query and parses it
     querystring = query
-    definition_list = json.loads(await search_query(querystring))['list']
+    definition_list = json.loads(await search_query(querystring))[LIST]
 
-    definition = definition_list[0]['definition']
-    example = definition_list[0]['example']
+    definition = definition_list[0][DEFINTION]
+    example = definition_list[0][EXAMPLE]
 
     await ctx.send("Definition of " + querystring + " :" + definition)
     await ctx.send("Example of " + querystring + " :" + example)
 
+# Parse the random word and display here
 @bot.command(name='random')
 async def random(ctx):
-    definition_list = json.loads(await search_random_word())['list']
+    definition_list = json.loads(await search_random_word())[LIST]
 
-    definition = definition_list[0]['definition']
-    example = definition_list[0]['example']
-    await ctx.send(" WORD " +definition_list[0]['word'])
+    definition = definition_list[0][DEFINTION]
+    example = definition_list[0][EXAMPLE]
+    await ctx.send("Random word -> " +definition_list[0]['word'])
     await ctx.send(definition)
     await ctx.send(example)
 
+# Check if the bot is running
 @bot.event
 async def on_ready():
     print("Nonce Bot is ready")
 
-
+# Run the bot
 bot.run('MzU3NjI1MzQ3MDUyNjY2OTEw.XvZd2A.p_YNVvyw281Z5h_BYH1itjAPTuA')
 
